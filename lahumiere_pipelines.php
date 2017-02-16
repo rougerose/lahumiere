ef5c73571dfb17ed15132bf51deffc5d188a603b
christophe le drean <christophe@rouge-rose.net>
2017-02-16 16:33:33 +0100

    scss: - webfonts - aliases

--- lahumiere_pipelines.php
+++ lahumiere_pipelines.php
@@ -3,6 +3,6 @@
 if (!defined('_ECRIRE_INC_VERSION')) return;
 
 function lahumiere_insert_head_css($flux) {
-    $flux .= '<link rel="stylesheet" href="'.find_in_path('css/lahumiere.css').'" type="text/css" />';
+    $flux .= '<link rel="stylesheet" href="'.find_in_path('css/application.css').'" type="text/css" />';
     return $flux;
 }
