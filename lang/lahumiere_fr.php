fdac1c19ef4a81af85cf8b7b4dc272d810ff993d
christophe le drean <christophe@rouge-rose.net>
2017-02-21 22:10:15 +0100

    recherche: work in progress

--- lang/lahumiere_fr.php
+++ lang/lahumiere_fr.php
@@ -4,5 +4,9 @@ if (!defined("_ECRIRE_INC_VERSION")) return;
 
 $GLOBALS[$GLOBALS['idx_lang']] = array(
   // R
+  'recherche_label' => "Rechercher",
+  'recherche_lien' => "Faire une recherche",
+  'recherche_placeholder' => "Votre recherche",
+  'recherche_submit' => "OK",
   'retour_accueil' => "Retour à la page d'accueil",
 );
