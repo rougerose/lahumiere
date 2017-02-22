4a81ec0409842af2282d291d7a5a4eaaf0e552d0
christophe le drean <christophe@rouge-rose.net>
2017-02-22 18:16:41 +0100

    filtres nom, prénom et prenom_nom

--- lahumiere_fonctions.php
+++ lahumiere_fonctions.php
@@ -18,3 +18,67 @@ function mini_html($texte) {
   $texte = preg_replace(",\n+,", "", $texte);
   return $texte;
 }
+
+
+
+/**
+ * Formater un prénom et un nom.
+ *
+ * fonction reprise de
+ * https://contrib.spip.net/Ameliorer-l-affichage-public-et-la
+ *
+ * @filtre
+ * @param  string $texte        Nom*Prénom
+ * @return string Prénom Nom    Prénom suivi d'une espace insécable Nom ou la chaîne d'entrée.
+ */
+function prenom_nom($texte) {
+  if (strstr($texte, "*")) {
+    if (prenom($texte) && nom($texte)) {
+      return prenom($texte)."&nbsp;".nom($texte);
+    }
+  }
+  else {
+    return $texte;
+  }
+}
+
+/**
+ * Formater un nom.
+ *
+ * fonction reprise de
+ * https://contrib.spip.net/Ameliorer-l-affichage-public-et-la
+ *
+ * @filtre
+ * @param  string $texte Nom*Prénom
+ * @return string $nom|$texte Le nom ou la chaîne complète.
+ */
+function nom($texte) {
+  if ($nom = strstr($texte, "*", true)) {
+    return trim($nom);
+  }
+  else {
+    return $texte;
+  }
+}
+
+
+/**
+ * Formater un prénom.
+ *
+ * fonction reprise de
+ * https://contrib.spip.net/Ameliorer-l-affichage-public-et-la
+ *
+ * @filtre
+ * @param  string $texte Nom*Prénom
+ * @return string $prenom|void Le prénom ou rien.
+ */
+function prenom($texte) {
+  if(strstr($texte,"*")) {
+    if($prenom = trim(preg_replace('/^(.*)\*(.*)/i','$2',$texte))) {
+      return trim($prenom);
+    }
+  }
+  else {
+    return "";
+  }
+}
