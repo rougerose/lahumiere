08322bede25e3ecc85aad2e405ff55ab01d84ebe
christophe le drean <christophe@rouge-rose.net>
2017-02-22 18:16:00 +0100

    débug

--- lahumiere_options.php
+++ lahumiere_options.php
@@ -4,3 +4,18 @@ if (!defined("_ECRIRE_INC_VERSION")) return;
 
 // zcore
 $GLOBALS['z_blocs'] = array('content', 'head', 'head_js', 'header', 'footer');
+
+
+// Développement
+
+// pas de cache
+define('_NO_CACHE', -1);
+
+// tous les logs
+define('_LOG_FILELINE',true);
+
+// pas de cache CSS et JS
+define('_INTERDIRE_COMPACTE_HEAD_ECRIRE', true);
+
+// toutes les erreurs dans SPIP
+define('SPIP_ERREUR_REPORT',E_ALL);
