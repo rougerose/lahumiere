51c8d576fee8badd5789baaa94c4e5303b1c1563
christophe le drean <christophe@rouge-rose.net>
2017-03-03 19:39:09 +0100

    navigation précédent/suivant:
    - ajout dépendance plugin critere_precedent_suivant
    - navigation pour composition article-artiste

--- lang/lahumiere_en.php
+++ lang/lahumiere_en.php
@@ -3,14 +3,11 @@
 if (!defined("_ECRIRE_INC_VERSION")) return;
 
 $GLOBALS[$GLOBALS['idx_lang']] = array(
-
   // A
-  'artist_about' => "About the artist",
-  'artist_work' => "Work",
-
-  // F
-  'footnotes_title' => "Footnotes",
-
+    'artist_about'    => "About the artist",
+    'artist_next'     => "Next <span class='c-pagination__text-hide'>artist</span>",
+    'artist_previous' => "Previous <span class='c-pagination__text-hide'>artist</span>",
+    'artist_work'     => "Work",
   // R
-  'retour_accueil' => "Back to homepage",
+    'retour_accueil'  => "Back to homepage",
 );
--- lang/lahumiere_fr.php
+++ lang/lahumiere_fr.php
@@ -3,18 +3,15 @@
 if (!defined("_ECRIRE_INC_VERSION")) return;
 
 $GLOBALS[$GLOBALS['idx_lang']] = array(
-
   // A
-  'artist_about' => "À propos de l'artiste",
-  'artist_work' => "Œuvres",
-
-  // F
-  'footnotes_title' => "Notes",
-
+    'artist_about'          => "À propos de l'artiste",
+    'artist_next'           => "<span class='c-pagination__text-hide'>Artiste </span>suivant",
+    'artist_previous'       => "<span class='c-pagination__text-hide'>Artiste </span>précédent",
+    'artist_work'           => "Œuvres",
   // R
-  'recherche_label' => "Rechercher",
-  'recherche_lien' => "Faire une recherche",
-  'recherche_placeholder' => "Votre recherche",
-  'recherche_submit' => "OK",
-  'retour_accueil' => "Retour à la page d'accueil",
+    'recherche_label'       => "Rechercher",
+    'recherche_lien'        => "Faire une recherche",
+    'recherche_placeholder' => "Votre recherche",
+    'recherche_submit'      => "OK",
+    'retour_accueil'        => "Retour à la page d'accueil",
 );
