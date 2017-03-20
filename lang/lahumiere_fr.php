dd21780dd438f3548bdc3c996122d6165173b625
christophe le drean <christophe@rouge-rose.net>
2017-03-20 15:25:21 +0100

    Article expositions
    - Compositions avec logo et sans logo
    - Export événement au format ics

--- lang/lahumiere_fr.php
+++ lang/lahumiere_fr.php
@@ -5,13 +5,16 @@ if (!defined("_ECRIRE_INC_VERSION")) return;
 $GLOBALS[$GLOBALS['idx_lang']] = array(
   // A
     'artist_about'          => "À propos de l'artiste",
-    'artist_next'           => "<span class='c-pagination__text-hide'>Artiste </span>suivant",
-    'artist_previous'       => "<span class='c-pagination__text-hide'>Artiste </span>précédent",
+    'artist_next'           => '<span class="c-pagination__text-hide">Artiste </span>suivant',
+    'artist_previous'       => '<span class="c-pagination__text-hide">Artiste </span>précédent',
     'artist_work'           => "Œuvres",
   // E
+    'exhibition_add_to_cal'    => 'Ajouter à mon calendrier',
     'exhibition_current'    => "Exposition en cours",
+    'exhibition_end'        => 'au @date@',
     'exhibition_past'       => "Exposition passée",
     'exhibition_upcoming'   => "Exposition à venir",
+    'exhibition_start'      => 'Du @date@',
   // N
     'nav_images'            => "images",
     'nav_list'              => "liste",
--- notes.md
+++ notes.md
@@ -6,3 +6,8 @@
 # Documentation
 - Ne pas faire d'article uniquement avec un descriptif et sans contenu #TEXTE => page article vide.
 - De plus, le descriptif ne doit pas contenir de liens, sinon cela casse le code html sur la page d'accueil et la page exposition (un `<a href>` ne peut contenir un autre `<a href>`).
+
+
+# Todo
+- partager un article
+- plugin metaplus
