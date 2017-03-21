1277cd04b41fde181382432386cacb1bc3906438
christophe le drean <christophe@rouge-rose.net>
2017-03-21 11:53:12 +0100

    Corrections lang

--- lang/lahumiere_fr.php
+++ lang/lahumiere_fr.php
@@ -1,33 +1,35 @@
 <?php
 
-if (!defined("_ECRIRE_INC_VERSION")) return;
+if (!defined('_ECRIRE_INC_VERSION')) return;
 
 $GLOBALS[$GLOBALS['idx_lang']] = array(
   // A
-    'artist_about'          => "À propos de l'artiste",
-    'artist_next'           => '<span class="c-pagination__text-hide">Artiste </span>suivant',
-    'artist_previous'       => '<span class="c-pagination__text-hide">Artiste </span>précédent',
-    'artist_work'           => "Œuvres",
+  'artist_about'          => 'À propos de l\'artiste',
+  'artist_next'           => '<span class="c-pagination__text-hide">Artiste </span>suivant',
+  'artist_previous'       => '<span class="c-pagination__text-hide">Artiste </span>précédent',
+  'artist_work'           => 'Œuvres',
   // E
-    'exhibition_add_to_cal' => 'Ajouter à mon calendrier',
-    'exhibition_current'    => "Exposition en cours",
-    'exhibition_end'        => 'au @date@',
-    'exhibition_next'       => '<span class="c-pagination__text-hide">Exposition </span>suivante',
-    'exhibition_past'       => "Exposition passée",
-    'exhibition_previous'   => '<span class="c-pagination__text-hide">Exposition </span>précédente',
-    'exhibition_upcoming'   => "Exposition à venir",
-    'exhibition_start'      => 'Du @date@',
+  'exhibition_add_to_cal' => 'Ajouter à mon calendrier',
+  'exhibition_current'    => 'Exposition en cours',
+  'exhibition_end'        => 'au @date@',
+  'exhibition_next'       => '<span class="c-pagination__text-hide">Exposition </span>suivante',
+  'exhibition_past'       => 'Exposition passée',
+  'exhibition_previous'   => '<span class="c-pagination__text-hide">Exposition </span>précédente',
+  'exhibition_upcoming'   => 'Exposition à venir',
+  'exhibition_start'      => 'Du @date@',
+  // G
+  'gallery_about'         => 'À propos de la Galerie',
   // N
-    'nav_images'            => "images",
-    'nav_list'              => "liste",
-    'nav_view'              => "Afficher",
-    'next'                  => "Suivant",
+  'nav_images'            => 'images',
+  'nav_list'              => 'liste',
+  'nav_view'              => 'Afficher',
+  'next'                  => 'Suivant',
   // P
-    'previous'              => "Précédent",
+  'previous'              => 'Précédent',
   // R
-    'recherche_label'       => "Rechercher",
-    'recherche_lien'        => "Faire une recherche",
-    'recherche_placeholder' => "Votre recherche",
-    'recherche_submit'      => "OK",
-    'retour_accueil'        => "Retour à la page d'accueil",
+  'recherche_label'       => 'Rechercher',
+  'recherche_lien'        => 'Faire une recherche',
+  'recherche_placeholder' => 'Votre recherche',
+  'recherche_submit'      => 'OK',
+  'retour_accueil'        => 'Retour à la page d\'accueil',
 );
